8564e7d54f5c3aade6929d5c132312f5315b3e26
konstantinwolters <kon1986@freenet.de>
2023-05-31 22:14:55 +0200

    Removed unnecessary classes

--- src/test/java/com/example/defecttrackerserver/core/user/UserServiceImplTest.java
+++ src/test/java/com/example/defecttrackerserver/core/user/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceImplTest2 {
+public class UserServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
