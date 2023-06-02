6b0bb84af968ac2191bb2348944b90c8afcbfee6
konstantinwolters <kon1986@freenet.de>
2023-06-02 21:31:23 +0200

    Adjust UserControllerTest to use @WebMvcTest

--- src/test/java/com/example/defecttrackerserver/core/user/UserControllerTest.java
+++ src/test/java/com/example/defecttrackerserver/core/user/UserControllerTest.java
@@ -1,11 +1,17 @@
 package com.example.defecttrackerserver.core.user;
 
+import com.example.defecttrackerserver.config.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,30 +22,50 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
 public class UserControllerTest {
 
-    @Mock
+    @MockBean
     private UserService userService;
 
-    @InjectMocks
-    private UserController userController;
-
+    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private UserDto testuserDto;
 
     @BeforeEach
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        testuserDto = new UserDto();
+        testuserDto.setUsername("bill");
     }
 
     @Test
-    @WithMockUser(username = "XXXXX", roles = "READ")
-    public void testGetUsers() throws Exception {
-        when(userService.getUser(anyInt())).thenReturn(new UserDto());
+    @WithMockUser(username = "bill", roles = "ADMIN")
+    public void testsaveUser() throws Exception {
+        when(userService.saveUser(any(UserDto.class))).thenReturn(testuserDto);
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(testuserDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value("bill"));
+
+        verify(userService, times(1)).saveUser(any(UserDto.class));
+    }
+    @Test
+    @WithMockUser(username = "bill", roles = "ADMIN")
+    public void testGetUser() throws Exception {
+        when(userService.getUser(anyInt())).thenReturn(testuserDto);
 
         mockMvc.perform(get("/users/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value("bill"));
 
         verify(userService, times(1)).getUser(anyInt());
     }
