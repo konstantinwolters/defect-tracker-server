23bc39cb3a9faaabbd6ffd1ba257ba30e2572e11
konstantinwolters <kon86@gmx.net>
2023-06-16 19:59:44 +0200

    Refactor UserDto so that only ids of assignedactions are referenced, not actiondtos

--- src/main/java/com/example/defecttrackerserver/config/ApplicationConfig.java
+++ src/main/java/com/example/defecttrackerserver/config/ApplicationConfig.java
@@ -1,10 +1,16 @@
 package com.example.defecttrackerserver.config;
 
+import com.example.defecttrackerserver.core.action.Action;
 import com.example.defecttrackerserver.core.user.user.User;
 import com.example.defecttrackerserver.core.user.user.dto.UserDto;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 public class ApplicationConfig {
@@ -14,9 +20,23 @@ public class ApplicationConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.typeMap(User.class, UserDto.class)
                 .addMappings( mapper -> {
-                    mapper.skip(UserDto::setPassword);
-                });
+                    mapper.skip(UserDto::setPassword);})
+                .addMappings(mapper -> mapper.using(actionToIdConverter).map(User::getAssignedActions, UserDto::setAssignedActions));
 
         return modelMapper;
     }
+
+
+
+    Converter<Set<Action>, Set<Integer>> actionToIdConverter = new AbstractConverter<Set<Action>, Set<Integer>>() {
+        @Override
+        protected Set<Integer> convert(Set<Action> source) {
+            if (source == null) {
+                return null;
+            }
+            return source.stream()
+                    .map(Action::getId)
+                    .collect(Collectors.toSet());
+        }
+    };
 }
--- src/main/java/com/example/defecttrackerserver/core/action/ActionDto.java
+++ src/main/java/com/example/defecttrackerserver/core/action/ActionDto.java
@@ -2,8 +2,6 @@ package com.example.defecttrackerserver.core.action;
 
 import com.example.defecttrackerserver.core.defect.defect.DefectDto;
 import com.example.defecttrackerserver.core.user.user.dto.UserDto;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +12,6 @@ import java.util.Set;
 
 @Getter
 @Setter
-@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class ActionDto {
     private Integer id;
     private String description;
