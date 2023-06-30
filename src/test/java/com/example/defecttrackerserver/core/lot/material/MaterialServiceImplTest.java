bc1f9f48839664e6e3dea2db8e7ea70ec024e46f
konstantinwolters <kon86@gmx.net>
2023-06-30 21:15:34 +0200

    Add MaterialServiceImplTest as unit tests for MaterialServiceImpl

---
+++ src/test/java/com/example/defecttrackerserver/core/lot/material/MaterialServiceImplTest.java
@@ -0,0 +1,110 @@
+package com.example.defecttrackerserver.core.lot.material;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MaterialServiceImplTest {
+
+    @Mock
+    private MaterialRepository materialRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private MaterialServiceImpl materialService;
+
+    private MaterialDto materialDto;
+    private Material material;
+
+    @BeforeEach
+    void setUp() {
+        materialDto = new MaterialDto();
+        materialDto.setId(1);
+        materialDto.setName("testName");
+
+        material = new Material();
+        material.setId(1);
+        material.setName("testName");
+    }
+
+    @Test
+    void shouldSaveMaterial() {
+        when(materialRepository.save(any(Material.class))).thenReturn(material);
+        when(modelMapper.map(any(Material.class), eq(MaterialDto.class))).thenReturn(materialDto);
+
+        MaterialDto result = materialService.saveMaterial(materialDto);
+
+        assertNotNull(result);
+        assertEquals(material.getName(), result.getName());
+        verify(materialRepository, times(1)).save(any(Material.class));
+    }
+
+    @Test
+    void shouldReturnMaterialById() {
+        when(materialRepository.findById(any(Integer.class))).thenReturn(Optional.of(material));
+        when(modelMapper.map(material, MaterialDto.class)).thenReturn(materialDto);
+
+        MaterialDto result = materialService.getMaterialById(1);
+
+        assertNotNull(result);
+        assertEquals(material.getId(), result.getId());
+        assertEquals(material.getName(), result.getName());
+    }
+
+    @Test
+    void shouldReturnAllMaterials(){
+        when(materialRepository.findAll()).thenReturn(Arrays.asList(material));
+        when(modelMapper.map(material, MaterialDto.class)).thenReturn(materialDto);
+
+        List<MaterialDto> result = materialService.getAllMaterials();
+
+        assertNotNull(result);
+        assertEquals(material.getId(), result.get(0).getId());
+        assertEquals(material.getName(), result.get(0).getName());
+    }
+
+    @Test
+    void shouldUpdateMaterial() {
+        when(materialRepository.findById(any(Integer.class))).thenReturn(Optional.of(material));
+        when(materialRepository.save(any(Material.class))).thenReturn(material);
+        when(modelMapper.map(any(Material.class), eq(MaterialDto.class))).thenReturn(materialDto);
+
+        MaterialDto result = materialService.updateMaterial(materialDto);
+
+        assertNotNull(result);
+        assertEquals(material.getId(), result.getId());
+        assertEquals(material.getName(), result.getName());
+        verify(materialRepository, times(1)).save(material);
+    }
+
+    @Test
+    void shouldDeleteMaterial() {
+        when(materialRepository.findById(any(Integer.class))).thenReturn(Optional.of(material));
+
+        materialService.deleteMaterial(1);
+
+        verify(materialRepository, times(1)).delete(material);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenMaterialNotFound(){
+        when(materialRepository.findById(any(Integer.class))).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> materialService.deleteMaterial(1));
+    }
+}
