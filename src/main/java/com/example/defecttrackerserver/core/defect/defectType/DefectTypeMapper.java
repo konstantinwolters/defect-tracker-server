97ff3ecae5480c26da891df46755853d5f1990db
konstantinwolters <kon86@gmx.net>
2023-07-02 18:37:30 +0200

    Remove ModelMapper from DefectType related classes and adjust DefectTypeMapper to do entity-to-dto-mapping

---
+++ src/main/java/com/example/defecttrackerserver/core/defect/defectType/DefectTypeMapper.java
@@ -0,0 +1,14 @@
+package com.example.defecttrackerserver.core.defect.defectType;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefectTypeMapper {
+
+    public DefectTypeDto mapToDto(DefectType defectType) {
+        DefectTypeDto defectTypeDto = new DefectTypeDto();
+        defectTypeDto.setId(defectType.getId());
+        defectTypeDto.setName(defectType.getName());
+        return defectTypeDto;
+    }
+}
