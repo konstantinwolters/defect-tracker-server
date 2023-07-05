2b35241288278025fa07740032910ad595d8a5e5
konstantinwolters <kon86@gmx.net>
2023-07-05 21:20:03 +0200

    Add endpoint that returns HTTPOnly Cookie with JWT Token

--- src/main/java/com/example/defecttrackerserver/core/auth/AuthenticationController.java
+++ src/main/java/com/example/defecttrackerserver/core/auth/AuthenticationController.java
@@ -1,6 +1,8 @@
 package com.example.defecttrackerserver.core.auth;
 
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,5 +21,15 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ){
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/authenticate-cookie")
+    public ResponseEntity<?> authenticationCookie(
+            @RequestBody AuthenticationRequest request
+    ) {
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.ok();
+        Cookie jwtCookie = authenticationService.createAuthenticationCookie(request);
+        responseBuilder.header(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+        return responseBuilder.build();
     }
 }
