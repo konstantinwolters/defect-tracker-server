c2f1ab97f008c9ac1234fd9688a88f587ee233bc
konstantinwolters <kon86@gmx.net>
2023-07-07 20:13:35 +0200

    Add new DefectControllermethod getFilteredDefects() to return list of defects according to various filter criteria

--- src/main/java/com/example/defecttrackerserver/core/defect/defect/DefectService.java
+++ src/main/java/com/example/defecttrackerserver/core/defect/defect/DefectService.java
@@ -5,6 +5,16 @@ import java.util.List;
 public interface DefectService {
     DefectDto saveDefect(DefectDto defectDto);
     DefectDto getDefectById(Integer id);
+    List<DefectDto> getFilteredDefects(
+            List<Integer> lotIds,
+            List<Integer> defectStatusIds,
+            String startDate,
+            String endDate,
+            List<Integer> locationIds,
+            List<Integer> processIds,
+            List<Integer> defectTypeIds,
+            List<Integer> createdByIds
+            );
     List<DefectDto> getAllDefects();
     DefectDto updateDefect(DefectDto defectDto);
     void deleteDefect(Integer id);
