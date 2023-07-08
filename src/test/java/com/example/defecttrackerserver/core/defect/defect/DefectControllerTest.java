0a1ed13c330569dce3086d85243f6549c2208b80
konstantinwolters <kon86@gmx.net>
2023-07-08 17:43:01 +0200

    Add abstract class BaseControllerTest for controller classes to addFilter for jwt authentication

--- src/test/java/com/example/defecttrackerserver/core/defect/defect/DefectControllerTest.java
+++ src/test/java/com/example/defecttrackerserver/core/defect/defect/DefectControllerTest.java
@@ -1,42 +1,75 @@
 package com.example.defecttrackerserver.core.defect.defect;
 
+import com.example.defecttrackerserver.BaseControllerTest;
 import com.example.defecttrackerserver.config.SecurityConfig;
+import com.example.defecttrackerserver.security.JwtAuthenticationFilter;
+import com.example.defecttrackerserver.security.JwtService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(DefectController.class)
-@Import(SecurityConfig.class)
-public class DefectControllerTest {
+public class DefectControllerTest extends BaseControllerTest {
+
+    @Autowired
+    private DefectController defectController;
+
+    @MockBean
+    UserDetailsService userDetailsService;
+
+    @MockBean
+    JwtService jwtService;
 
     @MockBean
     private DefectService defectService;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
     private DefectDto testDefectDto;
+
+    @Override
+    protected Object getController() {
+        return defectController;
+    }
+
+    @Override
+    protected JwtService getJwtService() {
+        return jwtService;
+    }
+
+    @Override
+    protected UserDetailsService getUserDetailsService() {
+        return userDetailsService;
+    }
 
     @BeforeEach
     public void setUp() {
+        super.setUp();
         testDefectDto = new DefectDto();
         testDefectDto.setLocation("Texas");
     }
@@ -47,6 +80,7 @@ public class DefectControllerTest {
         when(defectService.saveDefect(any(DefectDto.class))).thenReturn(testDefectDto);
 
         mockMvc.perform(post("/defects")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(testDefectDto)))
                 .andExpect(status().isOk())
@@ -59,7 +93,7 @@ public class DefectControllerTest {
     public void shouldGetDefectById() throws Exception {
         when(defectService.getDefectById(any(Integer.class))).thenReturn(testDefectDto);
 
-        mockMvc.perform(get("/defects/1"))
+        mockMvc.perform(get("/defects/1").with(csrf()).with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.location").value("Texas"));
@@ -70,7 +104,7 @@ public class DefectControllerTest {
     public void shouldGetAllDefects() throws Exception {
         when(defectService.getAllDefects()).thenReturn(Arrays.asList(testDefectDto));
 
-        mockMvc.perform(get("/defects"))
+        mockMvc.perform(get("/defects").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].location").value("Texas"));
@@ -81,6 +115,7 @@ public class DefectControllerTest {
     public void shouldUpdateDefect() throws Exception {
         when(defectService.updateDefect(any(DefectDto.class))).thenReturn(testDefectDto);
         mockMvc.perform(put("/defects")
+                        .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(testDefectDto)))
                 .andExpect(status().isOk())
@@ -92,7 +127,7 @@ public class DefectControllerTest {
     @WithMockUser(username = "bill", roles = "ADMIN")
     public void shouldDeleteDefect() throws Exception {
         doNothing().when(defectService).deleteDefect(any(Integer.class));
-        mockMvc.perform(delete("/defects/1"))
+        mockMvc.perform(delete("/defects/1").with(csrf()))
                 .andExpect(status().isOk());
     }
 }
