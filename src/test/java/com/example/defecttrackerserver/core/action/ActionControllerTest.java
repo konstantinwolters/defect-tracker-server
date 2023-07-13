97f1a601738d0bb51361a5cebd2120ea3a9bec7e
konstantinwolters <kon86@gmx.net>
2023-07-13 21:11:08 +0200

    Refactor ActionControllerTest to support paginated getAllActions() and getFilteredActions()

--- src/test/java/com/example/defecttrackerserver/core/action/ActionControllerTest.java
+++ src/test/java/com/example/defecttrackerserver/core/action/ActionControllerTest.java
@@ -1,15 +1,21 @@
 package com.example.defecttrackerserver.core.action;
 
 import com.example.defecttrackerserver.BaseControllerTest;
+import com.example.defecttrackerserver.response.PaginatedResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.doNothing;
@@ -65,27 +71,58 @@ public class ActionControllerTest extends BaseControllerTest {
                 .andExpect(jsonPath("$.description").value("test"));
     }
 
-//    @Test
-//    @WithMockUser(username = "bill", roles = "ADMIN")
-//    public void shouldGetAllActions() throws Exception {
-//        when(actionService.getAllActions()).thenReturn(Arrays.asList(testactionDto));
-//
-//        mockMvc.perform(get("/actions"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$[0].description").value("test"));
-//    }
+    @Test
+    @WithMockUser(username = "bill", roles = "ADMIN")
+    public void shouldGetAllActions() throws Exception {
+        Pageable pageable = PageRequest.of(0,10);
+        Page<ActionDto> page = new PageImpl<>(Arrays.asList(testactionDto));
+        PaginatedResponse<ActionDto> paginatedResponse = new PaginatedResponse<>(
+                Arrays.asList(testactionDto), 1 ,1 , 0);
 
-//    @Test
-//    public void shouldReturnFilteredActions() throws Exception{
-//        when(actionService.getFilteredActions(anyString(), anyString(), anyBoolean(), anyList(), anyList(), anyString(), anyString(), anyList()))
-//                .thenReturn(Arrays.asList(testactionDto));
-//
-//        mockMvc.perform(get("/actions/filtered")
-//                    .with(csrf())
-//                    .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//    }
+        when(actionService.getAllActions(pageable)).thenReturn(paginatedResponse);
+
+        mockMvc.perform(get("/actions?page=0&size=10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].description").value(testactionDto.getDescription()))
+                .andExpect(jsonPath("$.totalPages").value(page.getTotalPages()))
+                .andExpect(jsonPath("$.totalElements").value((int) page.getTotalElements()))
+                .andExpect(jsonPath("$.currentPage").value(page.getNumber()));
+    }
+
+    @Test
+    public void shouldReturnFilteredActions() throws Exception {
+        String dueDateStart = "2023-01-01";
+        String dueDateEnd = "2023-01-31";
+        Boolean isComplete = true;
+        List<Integer> assignedUserIds = Arrays.asList(1,2);
+        List<Integer> defectIds = Arrays.asList(1,2);
+        String createdOnStart = "2023-01-01";
+        String createdOnEnd = "2023-01-31";
+        List<Integer> createdByIds = Arrays.asList(1,2);
+        Pageable pageable = PageRequest.of(0,10);
+
+        PaginatedResponse<ActionDto> response = new PaginatedResponse<>(List.of(testactionDto), 1, 1, 0);
+
+        when(actionService.getFilteredActions(dueDateStart, dueDateEnd, isComplete, assignedUserIds, defectIds,
+                createdOnStart, createdOnEnd, createdByIds, pageable)).thenReturn(response);
+
+        mockMvc.perform(get("/actions/filtered")
+                        .param("dueDateStart", dueDateStart)
+                        .param("dueDateEnd", dueDateEnd)
+                        .param("isCompleted", String.valueOf(isComplete))
+                        .param("assignedUserIds", assignedUserIds.stream().map(Object::toString).toArray(String[]::new))
+                        .param("defectIds", defectIds.stream().map(Object::toString).toArray(String[]::new))
+                        .param("createdOnStart", createdOnStart)
+                        .param("createdOnEnd", createdOnEnd)
+                        .param("createdByIds", createdByIds.stream().map(Object::toString).toArray(String[]::new))
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].description").value(testactionDto.getDescription())) // Replace this with actual checks for your ActionDto fields
+                .andExpect(jsonPath("$.totalPages").value(response.getTotalPages()))
+                .andExpect(jsonPath("$.totalElements").value(response.getTotalElements()))
+                .andExpect(jsonPath("$.currentPage").value(response.getCurrentPage()));
+    }
 
     @Test
     @WithMockUser(username = "bill", roles = "ADMIN")
