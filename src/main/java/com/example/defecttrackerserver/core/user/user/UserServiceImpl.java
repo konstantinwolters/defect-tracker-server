0931602290b4d279b97914b4b3161d160c41c9fb
konstantinwolters <kon86@gmx.net>
2023-07-15 10:10:49 +0200

    Add authorization check for updateUser() method

--- src/main/java/com/example/defecttrackerserver/core/user/user/UserServiceImpl.java
+++ src/main/java/com/example/defecttrackerserver/core/user/user/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.defecttrackerserver.core.user.user;
 
+import com.example.defecttrackerserver.auth.authException.UnauthorizedAccessException;
 import com.example.defecttrackerserver.core.user.role.Role;
 import com.example.defecttrackerserver.core.user.role.RoleRepository;
+import com.example.defecttrackerserver.security.SecurityService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final RoleRepository roleRepository;
+    private final SecurityService securityService;
     private final PasswordEncoder passwordEncoder;
 
     @Override
@@ -50,12 +53,18 @@ public class UserServiceImpl implements UserService {
                 .toList();
     }
 
-    @Override // TODO: implement method security that only admins and the user themselves can update their own data
+    @Override
     @Transactional
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public UserDto updateUser(UserDto userDto) {
         User user = userRepository.findById(userDto.getId())
                 .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userDto.getId()));
+
+        if(!securityService.getUsername().equals(user.getUsername())
+        && !securityService.hasRole("ROLE_ADMIN")){
+            throw new UnauthorizedAccessException(
+                    "You are not authorized to update this user's data");
+        }
 
         if(userDto.getPassword() == null)
             userDto.setPassword(user.getPassword());
