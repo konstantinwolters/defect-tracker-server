d813f7bc07205f6312a7e75ce6146f3733c2e0fa
konstantinwolters <kon86@gmx.net>
2023-07-23 19:11:29 +0200

    Add unit test for ActionMapper.mapToDto() method

--- src/test/java/com/example/defecttrackerserver/core/action/ActionMapperTest.java
+++ src/test/java/com/example/defecttrackerserver/core/action/ActionMapperTest.java
@@ -4,6 +4,7 @@ import com.example.defecttrackerserver.core.defect.defect.Defect;
 import com.example.defecttrackerserver.core.defect.defect.DefectRepository;
 import com.example.defecttrackerserver.core.user.user.User;
 import com.example.defecttrackerserver.core.user.user.UserDto;
+import com.example.defecttrackerserver.core.user.user.UserMapper;
 import com.example.defecttrackerserver.core.user.user.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import net.bytebuddy.asm.Advice;
@@ -24,15 +25,17 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
 class ActionMapperTest {
-
-    @InjectMocks
-    private ActionMapper actionMapper;
-
     @Mock
     private DefectRepository defectRepository;
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private ActionMapper actionMapper;
 
     ActionDto actionDto;
     UserDto userDto;
@@ -95,6 +98,44 @@ class ActionMapperTest {
         when(userRepository.findById(any(Integer.class))).thenReturn(Optional.empty());
 
         assertThrows(EntityNotFoundException.class, () -> actionMapper.map(actionDto, new Action()));
+    }
+
+    @Test
+    void shouldReturnMappedActionDto() {
+        Action action = new Action();
+        action.setId(1);
+        action.setDescription("testDescription");
+        action.setDueDate(LocalDate.of(2023,1,1));
+        action.setIsCompleted(true);
+        action.setCreatedAt(LocalDateTime.now());
+        action.setChangedAt(LocalDateTime.now());
+
+        User user = new User();
+        user.setId(1);
+
+        action.setCreatedBy(user);
+        action.setAssignedUsers(Set.of(user));
+        action.setChangedBy(user);
+
+        Defect defect = new Defect();
+        defect.setId(1);
+        action.setDefect(defect);
+
+
+        when(userMapper.mapToDto(any(User.class))).thenReturn(userDto);
+
+        ActionDto mappedActionDto = actionMapper.mapToDto(action);
+
+        assertEquals(action.getId(), mappedActionDto.getId());
+        assertEquals(action.getDescription(), mappedActionDto.getDescription());
+        assertEquals(action.getIsCompleted(), mappedActionDto.getIsCompleted());
+        assertEquals(action.getCreatedAt(), mappedActionDto.getCreatedAt());
+        assertEquals(action.getChangedAt(), mappedActionDto.getChangedAt());
+        assertEquals(action.getDueDate(), mappedActionDto.getDueDate());
+        assertEquals(action.getDefect().getId(), mappedActionDto.getDefect());
+        assertEquals(action.getCreatedBy().getId(), (mappedActionDto.getCreatedBy().getId()));
+        assertEquals(action.getChangedBy().getId(), (mappedActionDto.getChangedBy().getId()));
+        assertEquals(action.getAssignedUsers().size(), mappedActionDto.getAssignedUsers().size());
     }
 }
 
