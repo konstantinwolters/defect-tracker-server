cf7f87eb9a784f25d381b68f2aa40ca2036dc894
konstantinwolters <kon86@gmx.net>
2023-07-30 19:22:20 +0200

    Add swagger annotations for all Process endpoints

--- src/main/java/com/example/defecttrackerserver/core/defect/process/ProcessController.java
+++ src/main/java/com/example/defecttrackerserver/core/defect/process/ProcessController.java
@@ -1,5 +1,7 @@
 package com.example.defecttrackerserver.core.defect.process;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +15,63 @@ import java.util.List;
 public class ProcessController {
     private final ProcessService processService;
 
+    @Operation(
+            summary = "Save Process",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Process saved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+            }
+    )
     @PostMapping
     public ProcessDto saveProcess(@Valid @RequestBody ProcessDto processDto) {
         return processService.saveProcess(processDto);
     }
 
+    @Operation(
+            summary = "Get Process by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Process found"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Process not found"),
+            }
+    )
     @GetMapping("/{id}")
     public ProcessDto getProcess(@PathVariable Integer id) {
         return processService.getProcessById(id);
     }
 
+    @Operation(
+            summary = "Get all Processes",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Processes found"),
+            }
+    )
     @GetMapping
     public List<ProcessDto> getAllProcesses() {
         return processService.getAllProcesses();
     }
 
+    @Operation(
+            summary = "Update Process",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Process updated successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Process not found"),
+            }
+    )
     @PutMapping("/{id}")
     public ProcessDto updateProcess(@PathVariable Integer id, @Valid @RequestBody ProcessDto processDto) {
         return processService.updateProcess(1, processDto);
     }
 
+    @Operation(
+            summary = "Delete Process",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Process successfully deleted"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Process not found"),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProcess(@PathVariable Integer id) {
         processService.deleteProcess(id);
