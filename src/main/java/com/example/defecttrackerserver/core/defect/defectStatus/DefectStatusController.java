b90f009da5feb2174a675cd91faa48ebf9d9e8fe
konstantinwolters <kon86@gmx.net>
2023-07-30 19:07:20 +0200

    Add swagger annotations for all DefectStatus endpoints

--- src/main/java/com/example/defecttrackerserver/core/defect/defectStatus/DefectStatusController.java
+++ src/main/java/com/example/defecttrackerserver/core/defect/defectStatus/DefectStatusController.java
@@ -1,5 +1,8 @@
 package com.example.defecttrackerserver.core.defect.defectStatus;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,29 +13,67 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/defectstatus")
+@Tag(name = "DefectStatus")
 public class DefectStatusController {
     private final DefectStatusService defectStatusService;
 
+    @Operation(
+            summary = "Add a DefectStatus",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "DefectStatus saved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+            }
+    )
     @PostMapping
     public DefectStatusDto saveDefectStatus(@Valid @RequestBody DefectStatusDto defectStatusDto) {
         return defectStatusService.saveDefectStatus(defectStatusDto);
     }
 
+    @Operation(
+            summary = "Get a DefectStatus by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "DefectStatus found"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "DefectStatus not found"),
+            }
+    )
     @GetMapping("/{id}")
     public DefectStatusDto getDefectStatus(@PathVariable Integer id) {
         return defectStatusService.getDefectStatusById(id);
     }
 
+    @Operation(
+            summary = "Get all DefectStatus",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "DefectStatus found"),
+            }
+    )
     @GetMapping
     public List<DefectStatusDto> getAllDefectStatus() {
         return defectStatusService.getAllDefectStatus();
     }
 
+    @Operation(
+            summary = "Update DefectStatus",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "DefectStatus updated successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "DefectStatus not found"),
+            }
+    )
     @PutMapping("/{id}")
     public DefectStatusDto updateDefectStatus(@PathVariable Integer id, @Valid @RequestBody DefectStatusDto defectStatusDto) {
         return defectStatusService.updateDefectStatus(1, defectStatusDto);
     }
 
+    @Operation(
+            summary = "Delete DefectStatus",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "DefectStatus successfully deleted"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "DefectStatus not found"),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDefectStatus(@PathVariable Integer id) {
         defectStatusService.deleteDefectStatus(id);
