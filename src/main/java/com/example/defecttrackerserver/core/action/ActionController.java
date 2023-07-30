96faa5558c43ee45298682f64617198ad1958f93
konstantinwolters <kon86@gmx.net>
2023-07-30 11:52:29 +0200

    Add exception handling for unsupported Http methods and typemismatch

--- src/main/java/com/example/defecttrackerserver/core/action/ActionController.java
+++ src/main/java/com/example/defecttrackerserver/core/action/ActionController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/actions")
-@Tag(name = "Action")
+@Tag(name = "Actions")
 public class ActionController {
     private final ActionService actionService;
 
