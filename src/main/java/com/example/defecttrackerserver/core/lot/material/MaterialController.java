33ffe6bb216922966f933f59455b06ca1801cde6
konstantinwolters <kon86@gmx.net>
2023-07-30 19:39:10 +0200

    Add Swagger annotation to all MaterialController endpoints

--- src/main/java/com/example/defecttrackerserver/core/lot/material/MaterialController.java
+++ src/main/java/com/example/defecttrackerserver/core/lot/material/MaterialController.java
@@ -1,5 +1,7 @@
 package com.example.defecttrackerserver.core.lot.material;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +15,63 @@ import java.util.List;
 public class MaterialController {
     private final MaterialService materialService;
 
+    @Operation(
+            summary = "Save Material",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Material saved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+            }
+    )
     @PostMapping
     public MaterialDto saveProcess(@Valid @RequestBody MaterialDto materialDto) {
         return materialService.saveMaterial(materialDto);
     }
 
+    @Operation(
+            summary = "Get Material by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Material found"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Material not found"),
+            }
+    )
     @GetMapping("/{id}")
     public MaterialDto getProcess(@PathVariable Integer id) {
         return materialService.getMaterialById(id);
     }
 
+    @Operation(
+            summary = "Get all Materials",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Material found"),
+            }
+    )
     @GetMapping
     public List<MaterialDto> getAllProcesses() {
         return materialService.getAllMaterials();
     }
 
+    @Operation(
+            summary = "Update Material",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Material updated successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Material not found"),
+            }
+    )
     @PutMapping("/{id}")
     public MaterialDto updateProcess(@PathVariable Integer id, @Valid @RequestBody MaterialDto materialDto) {
         return materialService.updateMaterial(id, materialDto);
     }
 
+    @Operation(
+            summary = "Delete Material",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Material successfully deleted"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Material not found"),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProcess(@PathVariable Integer id) {
         materialService.deleteMaterial(id);
