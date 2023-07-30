6c2bec31091c3fb6dceacaf3569c3d4809930c8a
konstantinwolters <kon86@gmx.net>
2023-07-30 19:27:58 +0200

    Add Swagger annotation to all LocationController endpoints

--- src/main/java/com/example/defecttrackerserver/core/location/LocationController.java
+++ src/main/java/com/example/defecttrackerserver/core/location/LocationController.java
@@ -1,5 +1,7 @@
 package com.example.defecttrackerserver.core.location;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +15,63 @@ import java.util.List;
 public class LocationController {
     private final LocationService locationService;
 
+    @Operation(
+            summary = "Save Location",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Location saved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+            }
+    )
     @PostMapping
     public LocationDto saveLocation(@Valid @RequestBody LocationDto locationDto) {
         return locationService.saveLocation(locationDto);
     }
 
+    @Operation(
+            summary = "Get Location by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Location saved successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Location not found"),
+            }
+    )
     @GetMapping("/{id}")
     public LocationDto getLocation(@PathVariable Integer id) {
         return locationService.getLocationById(id);
     }
 
+    @Operation(
+            summary = "Get all Locations",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Location saved successfully"),
+            }
+    )
     @GetMapping
     public List<LocationDto> getAllLocation() {
         return locationService.getAllLocations();
     }
 
+    @Operation(
+            summary = "Update Location",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Location updated successfully"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Location not found"),
+            }
+    )
     @PutMapping("/{id}")
     public LocationDto updateLocation(@PathVariable Integer id, @Valid@RequestBody LocationDto locationDto) {
         return locationService.updateLocation(1, locationDto);
     }
 
+    @Operation(
+            summary = "Delete Location",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Location successfully deleted"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input"),
+                    @ApiResponse(responseCode = "404", description = "Location not found"),
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteLocation(@PathVariable Integer id) {
         locationService.deleteLocation(id);
