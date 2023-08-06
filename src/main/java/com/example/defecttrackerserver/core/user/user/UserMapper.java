62179b98db2574455b44c19c85ebd4432508b226
konstantinwolters <kon86@gmx.net>
2023-08-06 19:01:41 +0200

    Refactor UserMapper and UserMapperTest to increase readability

--- src/main/java/com/example/defecttrackerserver/core/user/user/UserMapper.java
+++ src/main/java/com/example/defecttrackerserver/core/user/user/UserMapper.java
@@ -1,9 +1,12 @@
 package com.example.defecttrackerserver.core.user.user;
 
 import com.example.defecttrackerserver.core.action.Action;
+import com.example.defecttrackerserver.core.action.ActionDto;
 import com.example.defecttrackerserver.core.action.ActionRepository;
+import com.example.defecttrackerserver.core.location.Location;
 import com.example.defecttrackerserver.core.location.LocationRepository;
 import com.example.defecttrackerserver.core.user.role.Role;
+import com.example.defecttrackerserver.core.user.role.RoleDto;
 import com.example.defecttrackerserver.core.user.role.RoleRepository;
 import com.example.defecttrackerserver.core.user.user.userDtos.UserDto;
 import com.example.defecttrackerserver.core.user.user.userException.UserExistsException;
@@ -11,8 +14,10 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -26,6 +31,19 @@ public class UserMapper {
     public User mapToEntity(UserDto userDto, User user){
         checkDuplicateUserEntries(userDto);
 
+        Location location = getLocationByName(userDto.getLocation());
+        Set<Role> roles = Optional.ofNullable(userDto.getRoles())
+                .orElse(Collections.emptySet())
+                .stream()
+                .map(this::getRoleByName)
+                .collect(Collectors.toSet());
+
+        Set<Action> actions =  Optional.ofNullable(userDto.getAssignedActions())
+                .orElse(Collections.emptySet())
+                .stream()
+                .map(this::getActionById)
+                .collect(Collectors.toSet());
+
         user.setCustomId(userDto.getCustomId());
         user.setUsername(userDto.getUsername());
         user.setPassword(userDto.getPassword());
@@ -37,31 +55,46 @@ public class UserMapper {
         user.setChangedAt(userDto.getChangedAt());
         user.setCreatedBy(userDto.getCreatedBy());
         user.setChangedBy(userDto.getChangedBy());
+        user.setLocation(location);
+        user.setRoles(roles);
+        user.setAssignedActions(actions);
 
-        user.setLocation(locationRepository.findByName(
-                userDto.getLocation())
-                .orElseThrow(()-> new EntityNotFoundException("Location not found with name: "
-                        + userDto.getLocation())));
-
-        if(userDto.getRoles() != null){
-           user.setRoles(userDto.getRoles().stream()
-                   .map(role -> roleRepository.findByName(role)
-                           .orElseThrow(()-> new EntityNotFoundException("Role not found with name: "
-                                   + role)))
-                   .collect(Collectors.toSet()));
-        }
-
-        if(userDto.getAssignedActions() != null ) {
-            user.setAssignedActions(userDto.getAssignedActions().stream()
-                    .map(actionId -> actionRepository.findById(actionId)
-                            .orElseThrow(()-> new EntityNotFoundException("Action not found with id: "
-                                    + actionId)))
-                    .collect(Collectors.toSet()));
-        }
         return user;
     }
 
+    private Role getRoleByName(String roleName) {
+        return roleRepository.findByName(roleName)
+                .orElseThrow(()-> new EntityNotFoundException("Role not found with name: "
+                        + roleName));
+    }
+
+
+    private Action getActionById(Integer actionId) {
+        return actionRepository.findById(actionId)
+                .orElseThrow(()-> new EntityNotFoundException("Action not found with id: "
+                        + actionId));
+
+    }
+
+    private Location getLocationByName(String name){
+        return locationRepository.findByName(name)
+                .orElseThrow(()-> new EntityNotFoundException("Location not found with name: "
+                        + name));
+    }
+
     public UserDto mapToDto(User user){
+        Set<String> roles = Optional.ofNullable(user.getRoles())
+                .orElse(Collections.emptySet())
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.toSet());
+
+        Set<Integer> actions = Optional.ofNullable(user.getAssignedActions())
+                .orElse(Collections.emptySet())
+                .stream()
+                .map(Action::getId)
+                .collect(Collectors.toSet());
+
         UserDto userDto = new UserDto();
         userDto.setId(user.getId());
         userDto.setCustomId(user.getCustomId());
@@ -76,16 +109,9 @@ public class UserMapper {
         userDto.setChangedAt(user.getChangedAt());
         userDto.setCreatedBy(user.getCreatedBy());
         userDto.setChangedBy(user.getChangedBy());
-        if(user.getRoles() != null) {
-            userDto.setRoles(new HashSet<>(user.getRoles().stream()
-                    .map(Role::getName)
-                    .collect(Collectors.toSet())));
-        }
-        if(user.getAssignedActions() != null) {
-            userDto.setAssignedActions(new HashSet<>(user.getAssignedActions().stream()
-                    .map(Action::getId)
-                    .collect(Collectors.toSet())));
-        }
+        userDto.setRoles(roles);
+        userDto.setAssignedActions(actions);
+
         return userDto;
     }
 
