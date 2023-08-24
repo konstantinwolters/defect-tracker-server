254dfdfd276720afdd9a08b41a26016ebbb75c24
konstantinwolters <kon1986@freenet.de>
2023-08-24 23:47:26 +0200

    Add unit tests for AuthenticationController and AuthenticationService

--- src/main/java/com/example/defecttrackerserver/auth/AuthenticationController.java
+++ src/main/java/com/example/defecttrackerserver/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -25,13 +27,12 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ){
         return ResponseEntity.ok(authenticationService.authenticate(request));
-    }
+}
 
     @PostMapping("/refresh-token")
-    public void refreshToken(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws IOException {
-        authenticationService.refreshToken(request, response);
+    public ResponseEntity<Object> refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Optional<AuthenticationResponse> authResponse = authenticationService.refreshToken(request, response);
+        return authResponse.<ResponseEntity<Object>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).body((Object) "Unauthorized"));
     }
 }
---
+++ src/test/java/com/example/defecttrackerserver/auth/AuthenticationServiceImplTest.java
@@ -0,0 +1,121 @@
+package com.example.defecttrackerserver.auth;
+
+import com.example.defecttrackerserver.security.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthenticationServiceImplTest {
+
+    @Mock
+    private UserDetailsService userDetailsService;
+
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    private User userDetails;
+    private String authHeader;
+    private String username;
+    @BeforeEach
+    void setUp() {
+        userDetails = new User("username", "password", new ArrayList<>());
+        authHeader = "Bearer refreshToken";
+        username = "username";
+    }
+
+    @Test
+    void shouldAuthenticateRequest() {
+        AuthenticationRequest request = new AuthenticationRequest("username", "password");
+        String jwtToken = "jwtToken";
+        String refreshToken = "refreshToken";
+
+        when(userDetailsService.loadUserByUsername(request.getUsername())).thenReturn(userDetails);
+        when(jwtService.generateToken(userDetails)).thenReturn(jwtToken);
+        when(jwtService.generateRefreshToken(userDetails)).thenReturn(refreshToken);
+
+        AuthenticationResponse response = authenticationService.authenticate(request);
+
+        verify(authenticationManager).authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        assertEquals(jwtToken, response.getAccessToken());
+        assertEquals(refreshToken, response.getRefreshToken());
+    }
+
+    @Test
+    void testRefreshTokenWithValidHeader() throws IOException {
+        String accessToken = "newAccessToken";
+
+        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(authHeader);
+        when(jwtService.getUsernameFromToken("refreshToken")).thenReturn(username);
+        when(userDetailsService.loadUserByUsername(username)).thenReturn(userDetails);
+        when(jwtService.isTokenValid("refreshToken", userDetails)).thenReturn(true);
+        when(jwtService.generateToken(userDetails)).thenReturn(accessToken);
+
+        Optional<AuthenticationResponse> result = authenticationService.refreshToken(request, response);
+
+        assertTrue(result.isPresent());
+        assertEquals(accessToken, result.get().getAccessToken());
+        assertEquals("refreshToken", result.get().getRefreshToken());
+    }
+
+    @Test
+    void testRefreshTokenWithInvalidHeader() throws IOException {
+        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(null);
+
+        Optional<AuthenticationResponse> result = authenticationService.refreshToken(request, response);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testRefreshTokenWithInvalidToken() throws IOException {
+        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(authHeader);
+        when(jwtService.getUsernameFromToken("refreshToken")).thenReturn(null);
+
+        Optional<AuthenticationResponse> result = authenticationService.refreshToken(request, response);
+
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testRefreshTokenWithInvalidUser() throws IOException {
+        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(authHeader);
+        when(jwtService.getUsernameFromToken("refreshToken")).thenReturn(username);
+        when(userDetailsService.loadUserByUsername(username)).thenReturn(userDetails);
+        when(jwtService.isTokenValid("refreshToken", userDetails)).thenReturn(false);
+
+        Optional<AuthenticationResponse> result = authenticationService.refreshToken(request, response);
+
+        assertFalse(result.isPresent());
+    }
+}
