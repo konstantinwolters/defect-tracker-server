52277af1fc1424ff68ed3eacb3d5a8065cf94434
konstantinwolters <kon86@gmx.net>
2023-10-01 18:26:28 +0200

    Fix bug that was caused by NotificationAspect checking for the wrong object types

--- src/main/java/com/example/defecttrackerserver/notification/EmailService.java
+++ src/main/java/com/example/defecttrackerserver/notification/EmailService.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
