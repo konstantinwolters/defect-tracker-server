5a821a6adfcdc58b07695b447cf673d3446e018d
konstantinwolters <kon86@gmx.net>
2023-11-29 21:18:17 +0100

    Add statistics classes

---
+++ src/main/java/com/example/defecttrackerserver/statistics/DefectTypeCount.java
@@ -0,0 +1,11 @@
+package com.example.defecttrackerserver.statistics;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DefectTypeCount {
+    String name;
+    Integer count;
+}
---
+++ src/main/java/com/example/defecttrackerserver/statistics/ProcessCount.java
@@ -0,0 +1,11 @@
+package com.example.defecttrackerserver.statistics;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProcessCount {
+    String name;
+    Integer count;
+}
